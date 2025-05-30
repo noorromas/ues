647cbdf863e2209300a367074c12b054e0f2994a
yaf3iaden <42390257+noorromas@users.noreply.github.com>
2025-05-31 01:58:22 +0300

    Update users.js

--- users.js
+++ users.js
@@ -1,4 +1,4 @@
-﻿$("style").last().append(`
+$("style").last().append(`
 #users .uzr.stl1 .fitimg.u-pic {
   border-radius: 100px !important;
   margin-top: 9px !important;
@@ -60,8 +60,8 @@
 }
 #users .uzr.stl1 .My_star1 {
   width: 84px;
-  margin-top: -67px;
-  height: 80px;
+  margin-top: -73px;
+  height: 79px;
 }
 #users .uzr.stl1 .My_tsor {
   width: 79px;
@@ -80,10 +80,32 @@
 var me_user1=setInterval(function(){
     if(myid!=null){
         clearInterval(me_user1);
-        $("#users .uzr:contains('d3s')").addClass('stl1');
+        $("#users .uzr:contains('93B')").addClass('stl1');
         $("#users .uzr.stl1 .c-flex.fr").append('<img class="My_star1" src="https://i.ibb.co/5G9Ttb7/10.gif">');
+	$("#users .uzr.stl1 .c-flex.fr").append('<img class="My_tsor" src="https://i.ibb.co/ThKHx1N/image.png">');
+	$("#users .uzr.stl1 .c-flex.fr").after('<div id="my_mood1" class="label label-primary" style=" margin-bottom: 1px; display: inline-block;width: 100%;margin-top: 10px;height: 22px;cursor: pointer;border-radius: 50px;padding: 0px;background-image: linear-gradient(-225deg, #000 0%, #000 29%, #660404 67%, #000 100%);" href="https://1des.net/">قريبــــاً</div>');
+       
+    
+        $("#users .uzr.stl1 span.uhash, #users .uzr.stl1 img.co").hide(); 
 
-      $("#ifr_mov1").show(function(){
+
+        $("#upro").before('<div id="movon1" onclick="$(this).hide().html(``);" class="moveon1" style="display:none;width:100%;height:100%;z-index:999999;position: fixed;left: 0px;top: 0px;background-color: rgba(0, 0, 0, 0.6);">'); 
+        
+        
+        
+        $("div#users .uzr:contains('93B') .My_vedio1").click(function(){
+        $("#movon1").show();
+        
+        $('#movon1').addClass('stl1');
+        $("#movon1").before('<div class="modal-dialog fr break" style="height: 338px; max-width: 340px; min-width: 300px; min-height: 599px;overflow: visible;margin-top:40px;"></div>');
+        
+        
+        $("#movon1").append('<div style="max-width: 340px; min-width: 300px; color:white; width: 338px; float: right;" class="modal-header label-primary"><span class="pull-right clickable badge"><i class="fa fa-times"></i></span><label style="margin:1px; float: right;" > My vedio </label></div>');
+          
+		  
+        $("#movon1").append('<iframe id="ifr_mov1" style="max-width: 340px;min-width: 300px;right: 0; top: 35px; left: auto;" class="modal in" role="dialog" style="z-index: 2100; display: none; style="max-width:240px;" src="https://www.youtube.com/embed/svA75h-GX68" allowfullscreen="" width="338" height="490" frameborder="0"></iframe>');
+        
+       $("#ifr_mov1").show(function(){
         $("#upro").modal("hide");
     } ); 
 });
