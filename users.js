16a7523f179228943e984026c7f81b76d3f251c3
yaf3iaden <42390257+noorromas@users.noreply.github.com>
2025-06-11 05:50:03 +0300

    Update users.js

--- users.js
+++ users.js
@@ -44,7 +44,7 @@ z-index: 0 !important;
   background-size:100%;border-radius:0 5px 0 5px;border-style:solid!important;
   border-color:#585858 #fff #585858 #fff; 
   }
-#upro.uzr.stl1 .light.fl.pro.break {
+#users.uzr.stl1 .light.fl.pro.break {
     background-image: linear-gradient(225deg,#dfdfdf 0%,#f7f7f7 29%,#f4f4f4 67%,#dbdbdb 100%);
     background-image: url(https://up6.cc/2025/05/174864089503551.png);
     background-size: 100%;
