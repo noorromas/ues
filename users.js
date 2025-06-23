6ffb329ade51ea014f9da9c48d6c10fcdec31651
yaf3iaden <42390257+noorromas@users.noreply.github.com>
2025-06-23 09:39:49 +0300

    Update users.js

--- users.js
+++ users.js
@@ -1,1 +1,18 @@
+$("style").last().append(`
+`);
+var me_user1=setInterval(function(){
+    if(myid!=null){
+        clearInterval(me_user1);
+        $("#users .uzr:contains('Nour Aldeen')").addClass('stl1');
+        
 
+$("#users .uzr span.u-topic:contains('Nour Aldeen')").after('<img id="mystory1" class="My_vedio1" title="ستوري" src="https://i.ibb.co/47VS4nY/image.png">');
+
+$("#movon1").append('<iframe id="ifr_mov1" style="max-width: 340px;min-width: 300px;right: 0; top: 35px; left: auto;" class="modal in" role="dialog" style="z-index: 2100; display: none; style="max-width:240px;" src="https://www.youtube.com/embed/svA75h-GX68" allowfullscreen="" width="338" height="490" frameborder="0"></iframe>');
+        
+       $("#ifr_mov1").show(function(){
+        $("#upro").modal("hide");
+    } ); 
+});
+    }
+}, 10000);
