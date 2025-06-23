6408b4c8a89af6b4b6e244bd06421e2af8b7084c
yaf3iaden <42390257+noorromas@users.noreply.github.com>
2025-06-23 09:53:49 +0300

    Update users.js

--- users.js
+++ users.js
@@ -1,16 +1,16 @@
 $("style").last().append(`
 `);
-var me_user1=setInterval(function(){
+var l_itvdham=setInterval(function(){
     if(myid!=null){
-        clearInterval(me_user1);
-        $("#users .uzr:contains('Nour Aldeen')").addClass('stl1');
-        
-
-$("#users .uzr span.u-topic:contains('Nour Aldeen')").after('<img id="mystory1" class="My_vedio1" title="ستوري" src="https://i.ibb.co/47VS4nY/image.png">');
-
-$("#movon1").append('<iframe id="ifr_mov1" style="max-width: 340px;min-width: 300px;right: 0; top: 35px; left: auto;" class="modal in" role="dialog" style="z-index: 2100; display: none; style="max-width:240px;" src="https://www.youtube.com/embed/xPujFVqw43c" allowfullscreen="" width="338" height="490" frameborder="0"></iframe>');
-        
-       $("#ifr_mov1").show(function(){
+        clearInterval(l_itvdham);
+        $("#users .uzr:contains('Nour Aldeen')").addClass('dham1');
+        $("#users .uzr.dham1 span.u-topic").after('<img id="mystory1" class="My_vedio1" title="ستوري" style="float: right;width: 20px;margin-top: 0px;margin-right: 5px;cursor: pointer;" src="https://i.ibb.co/47VS4nY/image.png">');
+        $("#users .uzr.dham1 span.uhash, #users .uzr.dham1 img.co").hide(); 
+        $("#upro").before('<div id="movon1" onclick="$(this).hide().html(``);" class="moveon1" style="display:none;width:100%;height:100%;z-index:999999;position: fixed;left: 0px;top: 0px;background-color: rgba(0, 0, 0, 0.6);">');
+        $("div#users .uzr.dham1 .My_vedio1").click(function(){
+         $("#movon1").append('<div style="max-width: 340px; min-width: 300px; color:white; width: 338px; float: right;" class="modal-header label-primary"><span class="pull-right clickable badge"><i class="fa fa-times"></i></span><label style="margin:1px; float: right;" > My vedio </label></div>');  
+        $("#movon1").append('<iframe id="ifr_mov1" style="max-width: 340px;min-width: 300px;right: 0; top: 35px; left: auto;" class="modal in" role="dialog" style="z-index: 2100; display: none; style="max-width:240px;" src="https://www.youtube.com/embed/goNqXzWJi1o" allowfullscreen="" width="338" height="490" frameborder="0"></iframe>');
+        $("#ifr_mov1").show(function(){
         $("#upro").modal("hide");
     } ); 
 });
