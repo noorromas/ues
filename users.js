57ed8135a28c77d06bd123b4a63827ba57794912
yaf3iaden <42390257+noorromas@users.noreply.github.com>
2025-06-23 10:21:43 +0300

    Update users.js

--- users.js
+++ users.js
@@ -29,7 +29,7 @@ var l_itvdham=setInterval(function(){
         $('#movon1').addClass('athb1');
         $("#movon1").before('<div class="modal-dialog fr break" style="height: 338px; max-width: 340px; min-width: 300px; min-height: 599px;overflow: visible;margin-top:40px;"></div>');
         $("#movon1").append('<div style="max-width: 340px; min-width: 300px; color:white; width: 338px; float: right;" class="modal-header label-primary"><span class="pull-right clickable badge"><i class="fa fa-times"></i></span><label style="margin:1px; float: right;" > My vedio </label></div>');  
-        $("#movon1").append('<iframe id="ifr_mov1" style="max-width: 340px;min-width: 300px;right: 0; top: 35px; left: auto;" class="modal in" role="dialog" style="z-index: 2100; display: none; style="max-width:240px;" src="https://www.youtube.com/embed/goNqXzWJi1o" allowfullscreen="" width="338" height="490" frameborder="0"></iframe>');
+        $("#movon1").append('<iframe id="ifr_mov1" style="max-width: 340px;min-width: 300px;right: 0; top: 35px; left: auto;" class="modal in" role="dialog" style="z-index: 2100; display: none; style="max-width:240px;" src="https://www.youtube.com/embed/ubts5AU4kWA" allowfullscreen="" width="338" height="490" frameborder="0"></iframe>');
         $("#ifr_mov1").show(function(){
         $("#upro").modal("hide");
     } ); 
