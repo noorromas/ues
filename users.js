fa0dafbf8e96995ac2285496fc3906de5890b70b
yaf3iaden <42390257+noorromas@users.noreply.github.com>
2025-06-24 10:50:35 +0300

    Update users.js

--- users.js
+++ users.js
@@ -1,6 +1,11 @@
-
 $("style").last().append(`
-
+#users .uzr.stl1 .fitimg.u-pic {
+  border-radius: 100px !important;
+  margin-top: 9px !important;
+  height: 52px !important;
+  margin-left: 13px!important;
+  z-index: 0 !important;
+}
 #users .uzr.stl1 .d-flex.dots {
   margin-top: 7px !important;
   margin-left: 5px;
@@ -15,8 +20,65 @@ $("style").last().append(`
   color: transparent !important;
   background-size: 90% 100%;
 }
-
+#users .uzr.stl1 .ustat {
+  width: 54px !important;
+  border-radius: 0px 50px 50px 50px !important;
+  height: 52px !important;
+  margin-top: 5px !important;
+  min-height: 0% !important;
+  margin-left: 10px;
+  z-index: 0 !important;
+  display: none;
+}
+#users .uzr.stl1 span.uhash {
+  margin-right: 324px;
+  margin-top: 24px;
+  margin-left: 0px;
+}
+#users .uzr.stl1 img.co {
+  transform: rotate(90deg);
+  margin-top: -60px !important;
+  margin-right: 282px;
+}
+#users .uzr.stl1 {
+  box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 2px #000;
+  margin-bottom: 3px !important;
+  margin-top: 2px !important;
+  border-radius: 4px;
+  background-size: cover;
+  border: 1px solid #fff;
+}
+#users .uzr.stl1 .c-flex.fr {
+  margin-left: -4px!important;
+  float: left;
+}
+#users .uzr.stl1 .d-flex.fl {
+  padding-right: 0px !important;
+}
+#users .uzr.stl1 .u-topic {
+  margin-left: 5px;
+}
+#users .uzr.stl1 .My_star1 {
+  width: 84px;
+  margin-top: -67px;
+  height: 80px;
+}
+#users .uzr.stl1 .My_tsor {
+  width: 79px;
+  margin-top: -45px;
+  z-index: 0;
+  margin-left: 36px;
+}
+#users .uzr.stl1 .My_vedio1 {
+  float: right;
+  width: 20px;
+  margin-top: 0px;
+  margin-right: 5px;
+  cursor: pointer;
+}
 `);
+
+margin-right: 
 var l_itvdham=setInterval(function(){
     if(myid!=null){
         clearInterval(l_itvdham);
