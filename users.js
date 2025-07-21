793610b18ee8366b68a6e7e0efef67db590cbb6b
yaf3iaden <42390257+noorromas@users.noreply.github.com>
2025-07-21 17:14:06 +0300

    Update users.js

--- users.js
+++ users.js
@@ -1,44 +1,44 @@
 $("style").last().append(`
 #upro .border {
-      border-radius: 20px 0px 20px 0px!important;
+      border-radius: 10 0px 10 0px!important;
   border: 1px solid #bfbfbf;
 }
 #upro .modal-header.label-primary {
-      border-radius: 20px 0px 20px 0px!important;
+      border-radius: 10 0px 10 0px!important;
   border: 1px solid #ecdce4;
 }
 #d0 {
-      border-radius: 20px 0px 20px 0px!important;
+      border-radius: 10 0px 10 0px!important;
   border: 1px solid #ecdce41c;
   background-size: cover;
 }
 #d2 .label-primary{background-color:#b77c9a!important}#d2 .bg{background-color:#b77c9a!important}#d2 .uzr.d-flex.mm.hmsg{border-radius: 0 10px 0 10px;background-color:#d5d5d526!important}#d0 .nosel.fl.bg.{background-color:#b77c9a!important;  }#users span.uhash{text-transform:uppercase;background-image:linear-gradient(-225deg,#7b7b7b 0,#737373 29%,#ebebeb 67%,#3600ff 100%);background-size:220% auto;-webkit-background-clip:text;-webkit-text-fill-color:transparent;animation:2s linear infinite textclip;display:inline-block}
 #users .mini.u-msg{text-align:center;}#dpnl.bg{background-image:linear-gradient(225deg,#ce96b3 0%,#d091b1 29%,#eec6da 67%,#b77c9a 100%) */}#rooms .corner.fa.fa-user.label.uc.fa-microphone.label-danger{border-radius:5px!important}
 #rooms .label-primary {
-        border-radius: 20px 0px 20px 0px!important;
-    border: 1px solid #efefef00!important;
+        border-radius: 10 0px 10 0px!important;
+    border: 1px solid #efefef!important;
 }
-#users img.co{    border-radius: 20px 0px 20px 0px!important;transform: rotate(-55deg)margin-top:2px!important}
+#users img.co{    border-radius: 10 0px 10 0px!important;transform: rotate(-55deg)margin-top:2px!important}
 #users .nosel.ninr.fl.uzr.label.bg {
-        border-radius: 20px 0px 20px 0px!important;
+        border-radius: 10 0px 10 0px!important;
     background-size: cover;
 }
 #d0 .label-primary {
     border: 1px solid #e9e9e952 !important;
-        border-radius: 20px 0px 20px 0px!important;
+        border-radius: 10 0px 10 0px!important;
 }
 #settings .label-primary {
-      border-radius: 20px 0px 20px 0px!important;
+      border-radius: 10 0px 10 0px!important;
   border: 1px solid #ecdce4;
 }
 #settings .btn-primary {
-      border-radius: 20px 0px 20px 0px!important;
+      border-radius: 10 0px 10 0px!important;
   border: 1px solid #ecdce4;
 
 }
 #settings .bg-primary{background-color:#9d8980!important}#d0 .btn-primary{background-color:#9d8980!important}
 #users .tbox {
-        border-radius: 20px 0px 20px 0px!important;
+        border-radius: 10 0px 10 0px!important;
     background-color: #2525255c;
     border: 1px solid #efefef;
 }
@@ -47,16 +47,16 @@ $("style").last().append(`
     box-shadow: inset 0 0 0 rgba(0,0,0,.08), 0 0 2px #a21f1f;
 }
 #room #mic{
-    border-radius: 20px 0px 20px 0px!important;
+    border-radius: 10 0px 10 0px!important;
 }
 #d0 .nosel.fl .bg{background-image:linear-gradient(225deg,#ce96b3 0%,#d091b1 29%,#eec6da 67%,#b77c9a 100%)}#d0 .bg-primary{background-image:linear-gradient(225deg,#ce96b3 0%,#d091b1 29%,#eec6da 67%,#b77c9a 100%)}#d2 .label-primary{background-image:linear-gradient(225deg,#ce96b3 0%,#d091b1 29%,#eec6da 67%,#b77c9a 100%)}
 #room .btn-primary {
-        border-radius: 20px 0px 20px 0px!important;
+        border-radius: 10 0px 10 0px!important;
     border: 1px solid #ecdce447;
 }
 #d2{background-color:#fff !important;}
 #room .tablebox.d-flex.footer.fl.light {
-        border-radius: 20px 0px 20px 0px!important;
+        border-radius: 10 0px 10 0px!important;
     border: 1px solid #ecdce447;
 }
 #upro .light.fl.pro.break{background-image: linear-gradient(225deg,#dfdfdf 0%,#f7f7f7 29%,#f4f4f4 67%,#dbdbdb 100%);}
@@ -69,39 +69,39 @@ $("style").last().append(`
     box-shadow: 0 0 0px rgb(0 0 0), inset 0 0 4px rgb(0 0 0), 0 0 0 0px #000;
 }
 .corner.border.label.label-primary {
-      border-radius: 20px 0px 20px 0px!important;
+      border-radius: 10 0px 10 0px!important;
   border: 1px solid #ecdce4;
   background-color: #40404f!important;
   background-size: cover;
 }
 .modal-header.label-primary {
-	    border-radius: 20px 0px 20px 0px!important;
+	    border-radius: 10 0px 10 0px!important;
     border: 1px solid #ecdce4;
 }
 .head.d-flex.nosel.bg.fl, .rsave.btn.btn-primary.fr, .fa.fa-send.sndpm.fl.btn.btn-primary, .fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary, .fr.fa.fa-share-alt.sndfilebc.fl.btn.btn-primary, .btn-primary.fa.fa-comments, .bdel.corner.btn.minix.btn-primary.fa.fa-times, .fa.fa-send.sndbc.fl.btn.btn-primary {
-        border-radius: 20px 0px 20px 0px!important;
+        border-radius: 10 0px 10 0px!important;
     border: 1px solid #ecdce447;
 }
 .btn.btn-primary.u-nickc.fr.fa.fa-save {
-        border-radius: 20px 0px 20px 0px!important;
+        border-radius: 10 0px 10 0px!important;
     border: 1px solid #e7e7e7;
 
 }
 .label.fl.label-primary {
-        border-radius: 20px 0px 20px 0px!important;
+        border-radius: 10 0px 10 0px!important;
     border: 1px solid #e7e7e7;
 }
 
 input {
-      border-radius: 20px 0px 20px 0px!important;
+      border-radius: 10 0px 10 0px!important;
   border: 1px solid #ecdce4;
 }
 .tc {
-      border-radius: 20px 0px 20px 0px!important;
+      border-radius: 10 0px 10 0px!important;
   border: 1px solid #ecdce4;
 }
 .fl.borderg {
-      border-radius: 20px 0px 20px 0px!important;
+      border-radius: 10 0px 10 0px!important;
   border: 1px solid #bfbfbf;
 }
 .uhtml {
@@ -126,7 +126,7 @@ button.rsave.btn.btn-primary.fl {
 
 span.flex-grow-1.dots {
     text-align: center;
-        border-radius: 20px 0px 20px 0px!important;
+        border-radius: 10 0px 10 0px!important;
 }
 
 .dots {
@@ -152,7 +152,7 @@ div#l1 input#u1,div#l3 button {
 
 div#l2 button,div#l3 button {
     margin-left: 3px;
-        border-radius: 20px 0px 20px 0px!important;
+        border-radius: 10 0px 10 0px!important;
     height: 23px!important;
     padding: 0 5px
 }
@@ -163,7 +163,7 @@ div#l1 input#u1 {
     border: 1px solid #e1e0e0;
     margin-top: 3px!important;
     text-align: center;
-        border-radius: 20px 0px 20px 0px!important;
+        border-radius: 10 0px 10 0px!important;
 }
 
 div#l2 input#pass1,div#l2 input#u2,div#l3 input#pass2,div#l3 input#u3 {
@@ -179,14 +179,14 @@ div#l2 input#u2,div#l3 input#u3 {
     border-radius: 3px;
     border: 1px solid #e1e0e0;
     text-align: center;
-        border-radius: 20px 0px 20px 0px!important;
+        border-radius: 10 0px 10 0px!important;
 }
 
 div#l2 input#pass1,div#l3 input#pass2 {
     border-radius: 3px;
     border: 1px solid #e1e0e0;
     text-align: center;
-        border-radius: 20px 0px 20px 0px!important;
+        border-radius: 10 0px 10 0px!important;
 }
 
 div#l2,div#l3 {
@@ -208,7 +208,7 @@ div#l1,div#l2,div#l3 {
 
 div#l1 button {
     border: 1px solid #2c4666!important;
-        border-radius: 20px 0px 20px 0px!important;
+        border-radius: 10 0px 10 0px!important;
     width: 98%;
     height: 23px;
     margin-left: 3px;
@@ -224,7 +224,7 @@ ul.nav.nav-tabs>li>a {
     text-align: center;
     border-radius: 3px;
     padding: 0!important;
-    margin-right: 20px!important;
+    margin-right: 10!important;
     background-size: 300% 300%;
 }
 
@@ -284,7 +284,7 @@ a.label.label-primary.fl img.fl,button.btn.fr.btn-success.fa.fa-refresh,div#l2 b
 }
 
 a.label.label-primary.fl {
-        border-radius: 20px 0px 20px 0px!important;
+        border-radius: 10 0px 10 0px!important;
     margin: 2px -155px;
     padding: 2px!important;
     margin-bottom: 2px;
@@ -328,7 +328,7 @@ label.label.label-primary.mini.fl {
 }
 
 a.label.bg.d-flex.fl {
-        border-radius: 20px 0px 20px 0px!important;
+        border-radius: 10 0px 10 0px!important;
     margin: 1px 0px;
     padding: 1px!important;
     margin-bottom: 2px;
@@ -366,7 +366,7 @@ a.label.label-primary.fl img.fl,button.btn.fr.btn-success.fa.fa-refresh,div#l2 b
 
 a.label.label-primary.fl {
     height: 26px;
-        border-radius: 20px 0px 20px 0px!important;
+        border-radius: 10 0px 10 0px!important;
     margin: 2px -155px;
     padding: 2px!important;
     margin-bottom: 2px;
@@ -392,13 +392,6 @@ a.label.label-primary.fl img.fl {
     position: relative;
     color: #000
 }
-ul.nav.nav-tabs>li>a {
-	border: none;
-    padding: 0!important;
-    animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;
-    height: 22px !important;
-    line-height: 23px !important;
-}
 
 .fr.borderg {
     border: 0;
@@ -418,7 +411,7 @@ label.label.label-primary.mini.fl {
 
 
 .bg.mini.fl {
-        border-radius: 20px 0px 20px 0px!important;
+        border-radius: 10 0px 10 0px!important;
     margin-bottom: 5px;
 }
 .fitimg.u-pic.borderg {
@@ -431,7 +424,7 @@ label.label.label-primary.mini.fl {
 }
 
 label#loginstat {
-        border-radius: 20px 0px 20px 0px!important;
+        border-radius: 10 0px 10 0px!important;
 }
 
 div#tlogins ::-webkit-scrollbar {
@@ -497,7 +490,7 @@ border-radius: 50px;height: 52px !important;border:1px solid #fff;margin-top:2px
 
 
 div#tlogins a.label.bg.d-flex.fl img.fl {
-        border-radius: 20px 0px 20px 0px!important;
+        border-radius: 10 0px 10 0px!important;
     margin-top: -1px;
 }
 
@@ -509,15 +502,7 @@ div#tlogins {
     max-width: 394px!important;
 }
 
-div#wall .uzr.d-flex.mm {
-    width: 95% !important;
-    background: repeating-linear-gradient(356deg, rgba(255, 255, 255, .8), rgb(255, 255, 255) 100px, #ffffffb0 0, #fff6f699 200px), repeating-linear-gradient(359deg, rgba(255, 255, 255, .5), rgba(255, 255, 255, .93) 100px, #b3adadd1 0, #ffffffe6 200px);
-    color: #0f0202;
-    margin-left: 7px !important;
-    border-radius: 0 14px 0 14px;
-    box-shadow: 0 0 2px 0 #000;
-    margin-bottom: 5px !important;	
-}
+
 
 div {
     border-radius: 3px;
